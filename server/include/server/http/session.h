e51987f8e0f58e90beb130be7c533fd0e7e1b9e7
Ziga Bobnar <ziga@bobnar.net>
2019-12-28 10:27:21 +0100

    Add server session interface

--- server/include/server/http/session.h
+++ server/include/server/http/session.h
@@ -1,9 +1,28 @@
 #ifndef VORTEX_SERVER_HTTP_SESSION_H
 #define VORTEX_SERVER_HTTP_SESSION_H
 
+#include <boost/asio/ip/tcp.hpp>
+
 namespace vortex {
 namespace server {
 namespace http {
+
+class HttpSession : public std::enable_shared_from_this<HttpSession> {
+private:
+  boost::asio::ip::tcp::socket socket_;
+  boost::asio::strand<boost::asio::io_context::executor_type> strand_;
+  boost::beast::flat_buffer buffer_;
+  boost::beast::http::request<boost::beast::http::string_body> req_;
+  boost::beast::http::response<boost::beast::http::string_body> res_;
+public:
+  explicit HttpSession(boost::asio::ip::tcp::socket socket);
+  void run();
+  void do_read();
+  void on_read(boost::system::error_code ec, std::size_t bytes_transferred);
+  void on_write(boost::system::error_code ec, std::size_t bytes_transferred, bool close);
+  void do_close();
+  void send();
+}
 
 }  // namespace http
 }  // namespace server
