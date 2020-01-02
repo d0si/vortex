78166eb14b58b2103f3ffa93af6a9595c9950068
Ziga Bobnar <ziga@bobnar.net>
2020-01-02 19:07:07 +0100

    Implement redis cache on host

--- core/src/core/framework/host.cpp
+++ core/src/core/framework/host.cpp
@@ -9,9 +9,20 @@ host::host(framework* framework) : framework_(framework) {
 }
 
 void host::find(std::string hostname) {
-  host_ = framework_->mongo_
-    .get_collection("hosts")
-    .find_one(maze::object("hostname", hostname));
+  std::string redis_key = "vortex.core.host.value." + hostname;
+  if (framework_->redis_->exists(redis_key)) {
+    host_ = maze::object::from_json(framework_->redis_->get(redis_key));
+  }
+
+  if (host_.is_empty()) {
+    host_ = framework_->mongo_
+            .get_collection("hosts")
+            .find_one(maze::object("hostname", hostname));
+
+    if (!host_.is_empty()) {
+      framework_->redis_->set(redis_key, host_.to_json(0));
+    }
+  }
 
   if (host_.is_empty()) {
     framework_->view_.echo("Nothing is running on this hostname");
