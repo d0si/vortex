416138ba6654b0815fb95fdc2cbd30d7fc0520bb
Ziga Bobnar <ziga@bobnar.net>
2020-01-04 22:20:13 +0100

    Add view duktape methods and router interface

--- core/src/core/script/duktape.cpp
+++ core/src/core/script/duktape.cpp
@@ -17,16 +17,52 @@ public:
     framework_->view_.echo(content);
   }
 
+  void set_template(std::string name) {
+    framework_->view_.set_template(name);
+  }
+
+  void parse_template() {
+    framework_->view_.parse_template();
+  }
+
+  void set_page(std::string name) {
+    framework_->view_.set_page(name);
+  }
+
+  void parse_page() {
+    framework_->view_.parse_page();
+  }
+
   template<class Inspector>
   static void inspect(Inspector &i) {
     i.construct(&std::make_shared<view>);
     i.method("echo", &view::echo);
+    i.method("set_template", &view::set_template);
+    i.method("parse_template", &view::parse_template);
+    i.method("set_page", &view::set_page);
+    i.method("parse_page", &view::parse_page);
   }
 };
+
+class router {
+private:
+  vortex::core::framework::framework *framework_;
+
+public:
+  router() {}
+
+  router(vortex::core::framework::framework *framework) : framework_(framework) {}
+
+  template<class Inspector>
+  static void inspect(Inspector &i) {
+    i.construct(&std::make_shared<view>);
+  }
+}
 
 }  // namespace duktape_bindings
 
 DUK_CPP_DEF_CLASS_NAME(duktape_bindings::view);
+DUK_CPP_DEF_CLASS_NAME(duktape_bindings::router);
 
 namespace vortex {
 namespace core {
@@ -42,10 +78,12 @@ duktape::~duktape() {
 void duktape::setup() {
   ctx_->registerClass<duktape_bindings::view>();
   auto view = std::make_shared<duktape_bindings::view>(framework_);
+  auto router = std::make_shared<duktape_bindings::router>(framework_);
 
   ctx_->addGlobal("__view", view);
+  ctx_->addGlobal("__router", router);
 
-  exec("view=__view");
+  exec("view=__view;router=__router;");
 }
 
 void duktape::exec(std::string script) {
