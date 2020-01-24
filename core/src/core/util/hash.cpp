00e3efd6b687d3e664aada69ac2cd6c1e0172e40
Ziga Bobnar <ziga@bobnar.net>
2020-01-24 19:12:29 +0100

    Add md5 hash
    Related to #3

--- core/src/core/util/hash.cpp
+++ core/src/core/util/hash.cpp
@@ -1,6 +1,8 @@
 #include <core/util/hash.h>
 #include <cryptopp/sha.h>
 #include <cryptopp/hex.h>
+#define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
+#include <cryptopp/md5.h>
 
 namespace vortex {
 namespace core {
@@ -11,6 +13,22 @@ std::string sha1(std::string value) {
     CryptoPP::byte digest[CryptoPP::SHA1::DIGESTSIZE];
 
     CryptoPP::SHA1 hash;
+    hash.CalculateDigest(digest, (const CryptoPP::byte*) value.c_str(), value.length());
+
+    CryptoPP::HexEncoder encoder(nullptr, false);
+    std::string output;
+
+    encoder.Attach(new CryptoPP::StringSink(output));
+    encoder.Put(digest, sizeof(digest));
+    encoder.MessageEnd();
+
+    return output;
+}
+
+std::string md5(std::string value) {
+    CryptoPP::byte digest[CryptoPP::Weak::MD5::DIGESTSIZE];
+
+    CryptoPP::Weak::MD5 hash;
     hash.CalculateDigest(digest, (const CryptoPP::byte*) value.c_str(), value.length());
 
     CryptoPP::HexEncoder encoder(nullptr, false);
