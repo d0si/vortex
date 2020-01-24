839d45ac348ddc32cb307529e9b5937f532b8ce5
Ziga Bobnar <ziga@bobnar.net>
2020-01-24 19:08:01 +0100

    Add sha1 hash
    Related to #3

--- core/src/core/util/hash.cpp
+++ core/src/core/util/hash.cpp
@@ -1,9 +1,27 @@
 #include <core/util/hash.h>
+#include <cryptopp/sha.h>
+#include <cryptopp/hex.h>
 
 namespace vortex {
 namespace core {
 namespace util {
 namespace hash {
+
+std::string sha1(std::string value) {
+    CryptoPP::byte digest[CryptoPP::SHA1::DIGESTSIZE];
+
+    CryptoPP::SHA1 hash;
+    hash.CalculateDigest(digest, (const CryptoPP::byte*) value.c_str(), value.length());
+
+    CryptoPP::HexEncoder encoder(nullptr, false);
+    std::string output;
+
+    encoder.Attach(new CryptoPP::StringSink(output));
+    encoder.Put(digest, sizeof(digest));
+    encoder.MessageEnd();
+
+    return output;
+}
 
 }  // namespace hash
 }  // namespace util
