634b4453a72c86e24b39334122a03d70e0a73640
Ziga Bobnar <ziga@bobnar.net>
2020-02-24 17:35:53 +0100

    Add interfaces for filesystem based storage model

---
+++ core/include/core/storage/filesystem/collection.h
@@ -0,0 +1,16 @@
+#ifndef VORTEX_CORE_STORAGE_FILESYSTEM_COLLECTION_H
+#define VORTEX_CORE_STORAGE_FILESYSTEM_COLLECTION_H
+
+namespace Vortex {
+	namespace Core {
+		namespace Storage {
+			namespace Filesystem {
+				class Collection {
+
+				};
+			}  // namespace Mongo
+		}  // namespace Storage
+	}  // namespace Core
+}  // namespace Vortex
+
+#endif  // VORTEX_CORE_STORAGE_FILESYSTEM_COLLECTION_H
---
+++ core/include/core/storage/filesystem/database.h
@@ -0,0 +1,16 @@
+#ifndef VORTEX_CORE_STORAGE_FILESYSTEM_DB_H
+#define VORTEX_CORE_STORAGE_FILESYSTEM_DB_H
+
+namespace Vortex {
+	namespace Core {
+		namespace Storage {
+			namespace Filesystem {
+				class Db {
+
+				};
+			}  // namespace Filesystem
+		}  // namespace Storage
+	}  // namespace Core
+}  // namespace Vortex
+
+#endif  // VORTEX_CORE_STORAGE_FILESYSTEM_DB_H
