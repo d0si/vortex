61c66b06960dae532dfece5e2262ff27732c3895
Ziga Bobnar <ziga@bobnar.net>
2020-02-28 18:58:23 +0100

    Add dummy code to filesystem backend for testing purposes

--- core/src/Core/Storage/Filesystem/FilesystemBackend.cpp
+++ core/src/Core/Storage/Filesystem/FilesystemBackend.cpp
@@ -1,4 +1,5 @@
 #include <Core/Storage/Filesystem/FilesystemBackend.h>
+#include <maze/array.h>
 
 namespace Vortex {
 	namespace Core {
@@ -17,7 +18,33 @@ namespace Vortex {
 				}
 
 				std::string FilesystemBackend::find(std::string database, std::string collection, std::string query) {
-					return "";
+					maze::array results;
+
+					if (database == "vortex") {
+						if (collection == "hosts") {
+							maze::object host("hostname", "localhost");
+							host.set("app_id", "0");
+							host.set("script", "view.echo('Default host script.')");
+
+							results.push(host);
+						}
+						else if (collection == "apps") {
+							maze::object app("hostname", "localhost");
+							app.set("title", "Default application");
+							app.set("script", "view.echo('Default application script.')");
+
+							results.push(app);
+						}
+						else if (collection == "controllers") {
+							maze::object app("name", "index");
+							app.set("app_ids", maze::array().push(0));
+							app.set("script", "view.echo('Default controller script.')");
+
+							results.push(app);
+						}
+					}
+
+					return results.to_json();
 				}
 
 				void FilesystemBackend::update(std::string database, std::string collection, std::string query, std::string new_value) {
