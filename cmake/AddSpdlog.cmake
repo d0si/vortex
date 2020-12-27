358fe34720adc4a6d5b2eca2d904fc351487dcf1
Ziga Bobnar <ziga@bobnar.net>
2020-12-27 09:05:02 +0100

    Fix cmake cached options for spdlog

--- cmake/AddSpdlog.cmake
+++ cmake/AddSpdlog.cmake
@@ -6,9 +6,7 @@
 #
 # Add the spdlog project
 #
-cmake_policy(SET CMP0077 NEW) # This allows new policy to override option variables using normal variables
-set(SPDLOG_BUILD_SHARED ON)
-
+option(SPDLOG_BUILD_SHARED ON)
 add_subdirectory(${PROJECT_SOURCE_DIR}/../dependencies/spdlog ${CMAKE_CURRENT_BINARY_DIR}/spdlog)
 
 
